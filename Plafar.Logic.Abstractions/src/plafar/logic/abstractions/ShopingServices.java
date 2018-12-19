7ced6dd7f4a6652062e673d8f3bb5a817ff4a0cf
Petru Grozea <petrugrozea@gmail.com>
2018-12-19 22:24:19 +0200

    Changed ShopServices interface

--- Plafar.Logic.Abstractions/src/plafar/logic/abstractions/ShopingServices.java
+++ Plafar.Logic.Abstractions/src/plafar/logic/abstractions/ShopingServices.java
@@ -5,8 +5,9 @@ import plafar.domain.*;
 public interface ShopingServices {
 	StoreItem getItem(int itemId);
 	Bill sellItem(StoreItem item, int quantity);
-	boolean registerItem(StoreItem item, int quantity, float price);
+	boolean registerItem(StoreItem item);
 	boolean editItem(StoreItem item);
+	boolean deleteItem(int itemId);
 	
 	List<StoreItem> getAllItems();
 	List<StoreItem> getAvailableItems();
