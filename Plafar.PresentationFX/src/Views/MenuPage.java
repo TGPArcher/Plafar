79746240a8059a082f2432f18f2c1aa4ff21221a
Petru Grozea <petrugrozea@gmail.com>
2019-01-02 18:15:20 +0200

    Updated MenuPage
    * made it work with PageHandler
    * aligment fixes and general UI improvements, this is now the working wireframe of the page

--- Plafar.PresentationFX/src/Views/MenuPage.java
+++ Plafar.PresentationFX/src/Views/MenuPage.java
@@ -1,33 +1,87 @@
 package Views;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Separator;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
 public class MenuPage extends Page {
 
+	private VBox pageContent = new VBox();
+	
+	public MenuPage() {
+		setTitle("Main Menu");
+		pageContent.setFillWidth(true);
+	}
+	
 	@Override
 	public Parent doContents() {
 		// logo
-		ImageView image = new ImageView("file:C:/Users/TGP/Desktop/listsm.png");
+		addImage("file:C:/Users/TGP/Desktop/listsm.png", true);
+		
 		
 		// store list page
-		ImageView storeImage = new ImageView("file:C:/Users/TGP/Desktop/listsm.png");
-		Button storeButton = new Button();
-		storeButton.setGraphic(storeImage);
+		addButton("Store", "file:C:/Users/TGP/Desktop/listsm.png", true);
 		
 		// bill list page
-		ImageView billImage = new ImageView("file:C:/Users/TGP/Desktop/listsm.png");
-		Button billButton = new Button();
-		billButton.setGraphic(billImage);
+		addButton("Bill history", "file:C:/Users/TGP/Desktop/listsm.png", false);
 		
-		return new VBox(image,
-						new Separator(),
-						storeButton,
-						new Separator(),
-						billButton);
+		
+		return pageContent;
 	}
-
+	
+	private void addSeparator() {
+		Separator sep = new Separator();
+		sep.setPadding(new Insets(5));
+		
+		pageContent.getChildren().add(sep);
+	}
+	
+	private ImageView getImage(String graphicPath) {
+		ImageView img = new ImageView(graphicPath);
+		img.setFitWidth(100);
+        img.setPreserveRatio(true);
+        img.setSmooth(true);
+        img.setCache(true);
+        
+        return img;
+	}
+	
+	private void addImage(String graphicPath, boolean separator) {
+		if(graphicPath == null) {
+			return;
+		}
+		
+		BorderPane pane = new BorderPane();
+		pane.setCenter(getImage(graphicPath));
+		
+		pageContent.getChildren().add(pane);
+		if(separator) {
+			addSeparator();
+		}
+	}
+	
+	private void addButton(String text, String graphicPath, boolean separator) {
+		ImageView btnImage = null;
+		if(graphicPath != null) {
+			btnImage = getImage(graphicPath);
+		}
+		
+		Button btn = new Button();
+		if(text != null) {
+			btn.setText(text);
+		}
+		btn.setGraphic(btnImage);
+		btn.setAlignment(Pos.CENTER_LEFT);
+		btn.setMaxWidth(Double.MAX_VALUE);
+		
+		pageContent.getChildren().add(btn);
+		if(separator) {
+			addSeparator();
+		}
+	}
 }
