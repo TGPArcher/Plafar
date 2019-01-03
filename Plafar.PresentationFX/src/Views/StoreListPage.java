27cf0ed71693f1d09110199fcc0b71ba21b82a20
Petru Grozea <petrugrozea@gmail.com>
2019-01-03 17:04:56 +0200

    Updated StoreListPages: added edit, delete, add

--- Plafar.PresentationFX/src/Views/StoreListPage.java
+++ Plafar.PresentationFX/src/Views/StoreListPage.java
@@ -1,15 +1,37 @@
 package Views;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
+import plafar.domain.StoreItem;
 
 public class StoreListPage extends Page{
+	private List<StoreItem> items = null;
 	
+	private Button editBtn = null;
+	private Button deleteBtn = null;
+	
+	// prototype test only
 	public StoreListPage() {
+		setTitle("Store");
+		items = new LinkedList<StoreItem>();
+		items.add(new StoreItem(0, "Patlagina", "Cel mai lecuitor lucru ever", 3.56f, 9));
+		items.add(new StoreItem(0, "Curcunea", "Cel mai lecuitor lucru ever", 3.6f, 2));
+		items.add(new StoreItem(0, "Sires", "Ieftin bun si mult in kill", 1.99f, 99));
+		items.add(new StoreItem(0, "Patlajeli", "Siz di multi patlajeli", 0.56f, 200));
+	}
+	
+	public StoreListPage(List<StoreItem> items) {
+		this.items = items;
 		setTitle("Store");
 	}
 	
@@ -23,42 +45,67 @@ public class StoreListPage extends Page{
 	private Pane drawHeader() {
 		CheckBox available = new CheckBox("Available");
 		available.setSelected(true);
+		
 		CheckBox unavailable = new CheckBox("Unavailable");
 		unavailable.setSelected(true);
-		HBox headerPane = new HBox(10, available, unavailable);
+		
+		Region freeRegion = new Region();
+		HBox.setHgrow(freeRegion, Priority.ALWAYS);
+		
+		Button addBtn = new Button("Add");
+		addBtn.setAlignment(Pos.CENTER);
+		editBtn = new Button("Edit");
+		editBtn.setAlignment(Pos.CENTER);
+		editBtn.setDisable(true);
+		deleteBtn = new Button("Delete");
+		deleteBtn.setAlignment(Pos.CENTER);
+		deleteBtn.setDisable(true);
+		ButtonBar btns = new ButtonBar();
+		btns.getButtons().addAll(addBtn, editBtn, deleteBtn);
+		
+		HBox headerPane = new HBox(10, available, unavailable, freeRegion, btns);
+		headerPane.setPadding(new Insets(0, 5, 0, 0));
+		headerPane.setAlignment(Pos.CENTER);
 		
 		return new VBox(headerPane, new Separator());
 	}
 	
 	private ListView<HBox> drawItemList() {
-		ObservableList<HBox> items = FXCollections.observableArrayList();
-		for(int i = 0; i < 5; i++) {
-			items.add(drawItem());
+		ObservableList<HBox> itemsBox = FXCollections.observableArrayList();
+		for(int i = 0; i < items.size(); i++) {
+			itemsBox.add(drawItem(items.get(i)));
 		}
 		
-		ListView<HBox> list = new ListView<HBox>(items);
+		ListView<HBox> list = new ListView<HBox>(itemsBox);
 		list.setFixedCellSize(30);
-		VBox.setVgrow(list, Priority.ALWAYS);
+		list.getSelectionModel().selectedItemProperty().addListener(
+	            new ChangeListener<HBox>() {
+	                public void changed(ObservableValue<? extends HBox> ov, HBox old_val, HBox new_val) {
+	                        editBtn.setDisable(false);
+	                        deleteBtn.setDisable(false);
+	                }
+	            });
 		
+		VBox.setVgrow(list, Priority.ALWAYS);
 		return list;
 	}
 	
-	private HBox drawItem() {
+	private HBox drawItem(StoreItem item) {
 		HBox itemPane = new HBox(10);
 		itemPane.setAlignment(Pos.CENTER);
 		
-		Label name = new Label("Patlagina");
+		Label name = new Label(item.getName());
 		name.setAlignment(Pos.CENTER);
 		itemPane.getChildren().add(name);
 		
-		Label description = new Label("This is the description");
+		Label description = new Label(item.getDescription());
 		itemPane.getChildren().add(description);
 		
 		Region btnRegion = new Region();
 		HBox.setHgrow(btnRegion, Priority.ALWAYS);
 		itemPane.getChildren().add(btnRegion);
 		
-		Label price = new Label("5.55$");
+		Label price = new Label(String.valueOf(item.getPrice()) + " $");
 		price.setAlignment(Pos.CENTER);
 		itemPane.getChildren().add(price);
 		
