e74ccf771c6d6ca35dd2ef3c89ac0569cbfc9b20
Petru Grozea <petrugrozea@gmail.com>
2019-01-03 17:02:36 +0200

    Added support for a range of number for UnsignedIntegerFormatter

--- Plafar.PresentationFX/src/Formatters/UnsignedIntegerFormatter.java
+++ Plafar.PresentationFX/src/Formatters/UnsignedIntegerFormatter.java
@@ -16,4 +16,45 @@ public final class UnsignedIntegerFormatter {
 	     
 	     return new TextFormatter<String>(filter);
 	}
+	
+	public static TextFormatter<String> getFormatter(int max){
+		UnaryOperator<TextFormatter.Change> filter = change -> {
+			String newText = change.getControlNewText();
+		    if (newText.matches("\\d*?")) {
+		    	try {
+		    		if(Integer.parseInt(newText) <= max) {
+			    		return change;
+			    	}
+		    	}
+		    	catch(NumberFormatException e){
+		    		change.setText("");
+		    		return change;
+		    	}
+		    }
+		    return null;
+		};
+	     
+	     return new TextFormatter<String>(filter);
+	}
+	
+	public static TextFormatter<String> getFormatter(int min, int max){
+		UnaryOperator<TextFormatter.Change> filter = change -> {
+			String newText = change.getControlNewText();
+		    if (newText.matches("\\d*?")) {
+		    	try {
+		    		int newNr = Integer.parseInt(newText);
+			    	if(newNr >= min && newNr <= max) {
+			    		return change;
+			    	}
+		    	}
+		    	catch(NumberFormatException e) {
+		    		change.setText("");
+		    		return change;
+		    	}
+		    }
+		    return null;
+		};
+	     
+	     return new TextFormatter<String>(filter);
+	}
 }
