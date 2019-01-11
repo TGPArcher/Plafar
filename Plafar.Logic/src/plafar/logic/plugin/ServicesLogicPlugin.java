fb61162cba0f7484896b5b7e6e960d0ed1aa4d7d
Petru Grozea <petrugrozea@gmail.com>
2019-01-11 23:24:47 +0100

    Created Services Plugin for Logic

---
+++ Plafar.Logic/src/plafar/logic/plugin/ServicesLogicPlugin.java
@@ -0,0 +1,34 @@
+package plafar.logic.plugin;
+
+import plafar.logic.*;
+import plafar.logic.abstractions.*;
+import plafar.logic.abstractions.plugin.PlugableLogic;
+
+public class ServicesLogicPlugin implements PlugableLogic{
+
+	@Override
+	public String getName() {
+		return "Plafar.Logic.Services";
+	}
+	
+	@Override
+	public String getKey() {
+		return "Logic";
+	}
+
+	@Override
+	public String getVersion() {
+		return "0.0.1-SNAPSHOT";
+	}
+
+	@Override
+	public Class<? extends BillingService> getBillingServiceClass() {
+		return BillServices.class;
+	}
+
+	@Override
+	public Class<? extends ShopingServices> getShopingServiceClass() {
+		return ShopServices.class;
+	}
+
+}
