dad1c6506521d45ca8ab024a28f463457868a810
Petru Grozea <petrugrozea@gmail.com>
2019-01-11 23:25:09 +0100

    Created Plugin Manager for API

---
+++ Plafar.API/src/main/java/app/PluginLoader.java
@@ -0,0 +1,95 @@
+package app;
+
+import java.io.*;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import interfaces.Plugable;
+
+public class PluginLoader<T extends Plugable> {
+	private final Class<T> type;
+	private T plugin = null;
+	
+	public PluginLoader(Class<T> type, String directory) {
+		this.type = type;
+		searchForPlugins(directory);
+	}
+	
+	private void searchForPlugins(String directory) {
+		File pluginsDir = new File(directory);
+		FilenameFilter fileFilter = new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				if(name.endsWith(".jar")) {
+					return true;
+				}
+				
+				return false;
+			}
+		};
+		
+		for(File file : pluginsDir.listFiles(fileFilter)) {
+			plugin = loadPlugin(file);
+			if(plugin != null) {
+				return;
+			}
+		}
+		
+		if(plugin == null) {
+			System.err.println("ERROR: Could not find or load plugin!");
+		}
+	}
+	
+	@SuppressWarnings({ "unchecked" })
+	private T loadPlugin(File jar) {
+		T _plugin = null;
+		try {
+			URI jarUri = jar.toURI();
+			ClassLoader loader = URLClassLoader.newInstance(new URL[] {jarUri.toURL()});
+			List<String> classesInJar = getJarClasses(jar.getAbsolutePath());
+			
+			for(String className : classesInJar) {
+				Class<?> currentClass = Class.forName(className, true, loader);
+				if(type.isAssignableFrom(currentClass)) {
+					T currentPlugin = (T) currentClass.getConstructor().newInstance();
+					
+					// now search the configuration to match the condition
+					if(currentPlugin.getName().equals(PluginManager.getConfigKeyValue(currentPlugin.getKey()))) {
+						_plugin = currentPlugin;
+						break;
+					}
+				}
+			}
+		}
+		catch(Exception e) {
+			_plugin = null;
+		}
+		
+		return _plugin;
+	}
+	
+	private List<String> getJarClasses(String jarPath) throws FileNotFoundException, IOException {
+		List<String> classNames = new ArrayList<String>();
+		
+		ZipInputStream zip = new ZipInputStream(new FileInputStream(jarPath));
+		for(ZipEntry entry = zip.getNextEntry(); entry != null; entry = zip.getNextEntry()) {
+			if(!entry.isDirectory() && entry.getName().endsWith(".class")) {
+				String className = entry.getName().replace("/", ".");
+				classNames.add(className.substring(0, className.length() - ".class".length()));
+			}
+		}
+		if(zip != null) {
+			zip.close();
+		}
+		
+		return classNames;
+	}
+	
+	public T getPlugin() {
+		return plugin;
+	}
+}
---
+++ Plafar.API/src/main/java/app/PluginManager.java
@@ -0,0 +1,57 @@
+package app;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import plafar.logic.abstractions.plugin.PlugableLogic;
+import plafar.persistence.abstractions.plugin.PlugablePersistence;
+import util.JsonUtil;
+
+public final class PluginManager {
+	private static Dictionary<String, String> configuration = null;
+	private static PlugableLogic logicPlugin = null;
+	private static PlugablePersistence persistencePlugin = null;
+	
+	@SuppressWarnings("unchecked")
+	public static void loadConfiguration(String fileName) {
+		configuration = new Hashtable<String, String>();
+		
+		try {
+			String configPath = System.getProperty("user.dir") + "/config/" + fileName;
+			
+			byte[] encoded = Files.readAllBytes(Paths.get(configPath));
+			String configContents = new String(encoded, StandardCharsets.UTF_8);
+			configuration = JsonUtil.fromJson(configContents, Hashtable.class);
+		}
+		catch(IOException e) {
+			System.out.println("Could not load configuration!");
+			configuration = null;
+		}
+	}
+	
+	public static String getConfigKeyValue(String key) {
+		if(key == null) {
+			return null;
+		}
+		
+		return configuration.get(key);
+	}
+	
+	public static void loadPlugins() {
+		String pluginDirectoryPath = System.getProperty("user.dir") + "/plugins";
+		
+		logicPlugin = new PluginLoader<PlugableLogic>(PlugableLogic.class, pluginDirectoryPath).getPlugin();
+		persistencePlugin = new PluginLoader<PlugablePersistence>(PlugablePersistence.class, pluginDirectoryPath).getPlugin();
+	}
+	
+	public static PlugableLogic getLogicPlugin() {
+		return logicPlugin;
+	}
+	
+	public static PlugablePersistence getPersistencePlugin() {
+		return persistencePlugin;
+	}
+}
