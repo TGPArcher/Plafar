7480b80400ab29ac7417c0e563fec203abf5a270
Petru Grozea <petrugrozea@gmail.com>
2019-01-11 23:25:46 +0100

    Changed from static dependencies to dynamic using plugin manager

--- Plafar.API/src/main/java/app/CoreModule.java
+++ Plafar.API/src/main/java/app/CoreModule.java
@@ -2,19 +2,17 @@ package app;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.TypeLiteral;
-import plafar.logic.*;
 import plafar.logic.abstractions.*;
-import plafar.persistence.abastractions.*;
-import plafar.persistence.*;
+import plafar.persistence.abstractions.*;
 import plafar.domain.*;
 
 public class CoreModule extends AbstractModule {
 	@Override 
 	  protected void configure() {
-		bind(BillingService.class).to(BillServices.class);
-		bind(ShopingServices.class).to(ShopServices.class);
+		bind(BillingService.class).to(PluginManager.getLogicPlugin().getBillingServiceClass());
+		bind(ShopingServices.class).to(PluginManager.getLogicPlugin().getShopingServiceClass());
 		
-		bind(new TypeLiteral<Persistent<Bill>>() {}).to(BillRepository.class);
-		bind(new TypeLiteral<Persistent<StoreItem>>() {}).to(ItemRepository.class);
+		bind(new TypeLiteral<Persistent<Bill>>() {}).to(PluginManager.getPersistencePlugin().getBillPersistenceClass());
+		bind(new TypeLiteral<Persistent<StoreItem>>() {}).to(PluginManager.getPersistencePlugin().getItemPersistenceClass());
 	  }
 }
--- Plafar.API/src/main/java/app/MainAPI.java
+++ Plafar.API/src/main/java/app/MainAPI.java
@@ -9,10 +9,17 @@ import controllers.ShopController;
 public class MainAPI {
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
+		// setting api's port
 		port(5000);
 		
+		// loading the dependencies
+		PluginManager.loadConfiguration("config.json");
+		PluginManager.loadPlugins();
+		
+		// setting the dependency injector
 		Injector injector = Guice.createInjector(new CoreModule());
 		
+		// creating api's controllers
 		BillController billController = injector.getInstance(BillController.class);
 		ShopController shopController = injector.getInstance(ShopController.class);
 	}
